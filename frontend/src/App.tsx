a859ced094cac8f370b0e3212e485c70e3ce1083
Sanjay_Thakur <sanjay17126@gmail.com>
2025-01-27 01:05:54 +0530

    adding landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,15 @@ import './App.css'
 import Home from './pages/Home' 
 import LoginPage from './pages/Login'
 import SignupPage from './pages/Signup'
+import LandingPage from './pages/LandingPage'
 
 function App() {
 
   return (
    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<Home />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/signup' element={<SignupPage />} />
       </Routes> 
